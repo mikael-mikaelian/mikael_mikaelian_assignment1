3ebcc2b1b3943511387122c62ab24c81c54cf5e1
Mikael Mikaelian <mikaelmikaelian@Mikaels-iMac.local>
2022-09-20 21:48:34 +0300

    main.js added

---
+++ main.js
@@ -0,0 +1,43 @@
+//Question 1
+var myNumber;
+
+//Question 2
+myNumber = 5;
+
+//Question 3
+var myOtherNumber;
+myOtherNumber = 5;
+
+//Question 4
+var myTotal;
+myTotal = myNumber * myOtherNumber;
+
+//Question 5
+var myName = new String();
+var myCombo = new String();
+myCombo = (myNumber + myOtherNumber).toString();
+
+//Question 6
+var myOtherCombo;
+var myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+
+//Question 7
+var myRemainder;
+myRemainder = myNumber % 5;
+
+//Question 8
+var i = 2;
+
+//Question 9
+var int = 4;
+
+//Question 10
+var myBoolean = new Boolean();
+myBoolean = false;
+
+if(myBoolean == true)
+{
+    console.log("hello");
+}
+
